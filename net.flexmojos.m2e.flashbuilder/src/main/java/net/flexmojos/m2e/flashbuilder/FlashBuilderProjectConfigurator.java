16e868706031cb2dd297596448d9b5c76bfa800b
sylvainlecoy <sylvain.lecoy@gmail.com>
2013-01-18 18:35:36 +0100

    Configurator.

---
+++ net.flexmojos.m2e.flashbuilder/src/main/java/net/flexmojos/m2e/flashbuilder/FlashBuilderProjectConfigurator.java
@@ -0,0 +1,18 @@
+package net.flexmojos.m2e.flashbuilder;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
+import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
+
+public class FlashBuilderProjectConfigurator extends
+		AbstractProjectConfigurator {
+
+	@Override
+	public void configure(ProjectConfigurationRequest arg0,
+			IProgressMonitor arg1) throws CoreException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
