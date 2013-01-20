a5efd4797825a34096774f204711d9b9fad281c4
sylvainlecoy <sylvain.lecoy@gmail.com>
2013-01-20 23:27:58 +0100

    Added lifecycle mapping metadata.

--- net.flexmojos.m2e.flashbuilder/src/main/java/net/flexmojos/m2e/flashbuilder/FlashBuilderProjectConfigurator.java
+++ net.flexmojos.m2e.flashbuilder/src/main/java/net/flexmojos/m2e/flashbuilder/FlashBuilderProjectConfigurator.java
@@ -5,14 +5,18 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
 import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
 
+/**
+ * Used to configure a FlashBuilder project from Maven.
+ * 
+ * @author Sylvain Lecoy (sylvain.lecoy@gmail.com)
+ *
+ */
 public class FlashBuilderProjectConfigurator extends
 		AbstractProjectConfigurator {
 
 	@Override
-	public void configure(ProjectConfigurationRequest arg0,
-			IProgressMonitor arg1) throws CoreException {
-		// TODO Auto-generated method stub
-		
+	public void configure(ProjectConfigurationRequest request,
+			IProgressMonitor monitor) throws CoreException {
 	}
 
 }
