889e2b65e6f143621effce4349b5477a027cab4e
sylvainlecoy <sylvain.lecoy@gmail.com>
2013-01-21 19:40:23 +0100

    Added basic IProject integration.

--- net.flexmojos.m2e.flashbuilder/src/main/java/net/flexmojos/m2e/flashbuilder/FlashBuilderProjectConfigurator.java
+++ net.flexmojos.m2e.flashbuilder/src/main/java/net/flexmojos/m2e/flashbuilder/FlashBuilderProjectConfigurator.java
@@ -1,7 +1,14 @@
 package net.flexmojos.m2e.flashbuilder;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.apache.maven.project.MavenProject;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.m2e.core.project.IProjectConfigurationManager;
 import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
 import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
 
@@ -17,8 +24,21 @@ public class FlashBuilderProjectConfigurator extends
 	@Override
 	public void configure(ProjectConfigurationRequest request,
 			IProgressMonitor monitor) throws CoreException {
-		// Debugger correctly stop on breakpoint when Right-clicking on a
-		// flexmojos-maven-plugin driven Maven project.
+		IProject project = request.getProject();
+		IProjectDescription description = project.getDescription();
+		String[] natures = description.getNatureIds();
+		
+		if (!Arrays.asList(natures).contains("com.adobe.flexbuilder.project.flexnature")) {
+			Arrays.asList(natures).add("com.adobe.flexbuilder.project.flexnature");
+		}
+		
+		if (!Arrays.asList(natures).contains("com.adobe.flexbuilder.project.actionscriptnature")) {
+			Arrays.asList(natures).add("com.adobe.flexbuilder.project.actionscriptnature");
+		}
+		
+		description.setNatureIds(natures);
+		project.setDescription(description, monitor);
+		
 	}
 
 }
